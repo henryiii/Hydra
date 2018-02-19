de8ee1af8c3b8fd8fe8a7d7e47fdb901de9c4de1
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-02-19 15:12:46 +0100

    New version of CMake scripts

--- cmake/AddHydraExample.cmake
+++ cmake/AddHydraExample.cmake
@@ -1,75 +1,59 @@
+# This allows Add Hydra example to work with either a pair of .cpp and .cu
+# files, or a single .cu file for all backends!
+function(HYDRA_NORMALIZE INVAR OUTNAME)
+    if(EXISTS "${INVAR}.cpp")
+        set(${OUTNAME} "${INVAR}.cpp" PARENT_SCOPE)
+    else()
+        set(${OUTNAME} "${INVAR}.cu" PARENT_SCOPE)
+        set_source_files_properties("${INVAR}.cu" PROPERTIES LANGUAGE CXX)
+    endif()
+endfunction()
+
+# Add an example. Give the filename without the extension (.cu or .cpp)
+# Builds all available (requested) backends
 function(ADD_HYDRA_EXAMPLE target_name)
-        message(STATUS "-----------")
-        #+++++++++++++++++++++++++
-        # CUDA TARGETS           |
-        #+++++++++++++++++++++++++
-        if(BUILD_CUDA_TARGETS)
-                  message(STATUS "Adding target ${target_name} to CUDA backend. Executable file name: ${target_name}_cuda")
-                  
-                  cuda_add_executable("${target_name}_cuda"
-                   #EXCLUDE_FROM_ALL 
-                   "${target_name}.cu"    
-                   OPTIONS -Xcompiler -DTHRUST_DEVICE_SYSTEM=THRUST_DEVICE_SYSTEM_CUDA  -DTHRUST_HOST_SYSTEM=THRUST_HOST_SYSTEM_CPP)
-                    
-                  target_link_libraries("${target_name}_cuda" ${ROOT_LIBRARIES} )
-                 
-                  add_dependencies(examples      "${target_name}_cuda")
-                
-        endif(BUILD_CUDA_TARGETS)
-    
-        #+++++++++++++++++++++++++
-        # TBB TARGETS            |
-        #+++++++++++++++++++++++++
-        if(BUILD_TBB_TARGETS)
-                 message(STATUS "Adding target ${target_name} to TBB backend. Executable file name: ${target_name}_tbb")
-                 add_executable("${target_name}_tbb"
-                 # EXCLUDE_FROM_ALL
-                 "${target_name}.cpp" )
-                    
-                 set_target_properties( "${target_name}_tbb" 
-                  PROPERTIES COMPILE_FLAGS "-DTHRUST_HOST_SYSTEM=THRUST_HOST_SYSTEM_CPP -DTHRUST_DEVICE_SYSTEM=THRUST_DEVICE_SYSTEM_TBB")
-                    
-                 target_link_libraries( "${target_name}_tbb" ${ROOT_LIBRARIES} ${TBB_LIBRARIES} )
-                   
-                 add_dependencies(examples "${target_name}_tbb")
-                       
-         endif(BUILD_TBB_TARGETS)
-         
-        #+++++++++++++++++++++++++
-        # CPP TARGETS            |
-        #+++++++++++++++++++++++++
-        if(BUILD_CPP_TARGETS)
-                 message(STATUS "Adding target ${target_name} to CPP backend. Executable file name: ${target_name}_cpp")
-                 add_executable("${target_name}_cpp"
-                 # EXCLUDE_FROM_ALL 
-                 "${target_name}.cpp" )
-                    
-                 set_target_properties( "${target_name}_cpp" 
-                  PROPERTIES COMPILE_FLAGS "-DTHRUST_HOST_SYSTEM=THRUST_HOST_SYSTEM_CPP -DTHRUST_DEVICE_SYSTEM=THRUST_DEVICE_SYSTEM_CPP")
-                    
-                 target_link_libraries( "${target_name}_cpp" ${ROOT_LIBRARIES} ${TBB_LIBRARIES} )
-                   
-                 add_dependencies(examples "${target_name}_cpp")
-                       
-         endif(BUILD_CPP_TARGETS)
-         
-          
-        #+++++++++++++++++++++++++
-        # OMP TARGETS            |
-        #+++++++++++++++++++++++++
-        if(BUILD_OMP_TARGETS)
-                 message(STATUS "Adding target ${target_name} to OMP backend. Executable file name: ${target_name}_omp")
-                 add_executable("${target_name}_omp" 
-                 #EXCLUDE_FROM_ALL
-                 "${target_name}.cpp" )
-                    
-                 set_target_properties( "${target_name}_omp" 
-                  PROPERTIES COMPILE_FLAGS "-DTHRUST_HOST_SYSTEM=THRUST_HOST_SYSTEM_CPP -DTHRUST_DEVICE_SYSTEM=THRUST_DEVICE_SYSTEM_OMP ${OpenMP_CXX_FLAGS}")
-                    
-                 target_link_libraries( "${target_name}_omp" ${ROOT_LIBRARIES} ${OpenMP_CXX_LIBRARIES})
-                   
-                 add_dependencies(examples "${target_name}_omp")
-                       
-         endif(BUILD_OMP_TARGETS)
-         
-endfunction(ADD_HYDRA_EXAMPLE)  
+    set(ADDINGMESSAGE "Adding:")
+
+    hydra_normalize(${target_name} target_norm)
+
+    # CPP
+    if(BUILD_CPP_TARGETS)
+        set(ADDINGMESSAGE "${ADDINGMESSAGE} ${target_name}_cpp")
+        add_executable("${target_name}_cpp" "${target_norm}")
+        target_link_libraries("${target_name}_cpp" PUBLIC ROOT_lib tclap_lib Hydra_cpp)
+        target_compile_options(${target_name}_cpp PUBLIC -x c++)
+
+    endif()
+
+    # CUDA
+    if(BUILD_CUDA_TARGETS)
+        set(ADDINGMESSAGE "${ADDINGMESSAGE} ${target_name}_cuda")
+        
+        # Add the .cu file if one exists
+        cuda_add_executable("${target_name}_cuda"
+            "${target_name}.cu"    
+            OPTIONS -Xcompiler -DTHRUST_DEVICE_SYSTEM=THRUST_DEVICE_SYSTEM_CUDA  -DTHRUST_HOST_SYSTEM=THRUST_HOST_SYSTEM_CPP)
+
+        target_link_libraries("${target_name}_cuda" ROOT_lib tclap_lib Hydra_cuda )
+    endif()
+
+    # TBB
+    if(BUILD_TBB_TARGETS)
+        set(ADDINGMESSAGE "${ADDINGMESSAGE} ${target_name}_tbb")
+        add_executable("${target_name}_tbb" "${target_norm}")
+
+        target_link_libraries("${target_name}_tbb" PUBLIC ROOT_lib tclap_lib Hydra_tbb)
+        target_compile_options(${target_name}_tbb PUBLIC -x c++)
+    endif()
+
+    # OMP
+    if(BUILD_OMP_TARGETS)
+        set(ADDINGMESSAGE "${ADDINGMESSAGE} ${target_name}_omp")
+        add_executable("${target_name}_omp" "${target_norm}" )
+
+        target_link_libraries( "${target_name}_omp" PUBLIC ROOT_lib tclap_lib Hydra_omp)
+        target_compile_options(${target_name}_omp PUBLIC -x c++)
+    endif()
+
+    message(STATUS ${ADDINGMESSAGE})
+endfunction()
